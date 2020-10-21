ecf0166a18b6e2510bd47291ebd3c50fa9e225e0
ddesmond1 <ddesmond1@yahoo.com>
2020-10-21 10:24:33 -0400

    My initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let newDice1Value = Int.random(in: 0..<6)
+        let newDice2Value = Int.random(in: 0..<6)
+        
+        //change buttons
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][newDice1Value]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][newDice2Value]
+        
+        print("Button got tapped")
+    }
 }
 
